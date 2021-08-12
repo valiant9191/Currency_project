02f89fc93574de61190790af7702a818525f7903
Kolko Valentin <72280491+valiant9191@users.noreply.github.com>
2021-08-12 17:20:18 +0300

    Update NewsBox.js

--- src/components/headerNewsMenu/NewsBox/NewsBox.js
+++ src/components/headerNewsMenu/NewsBox/NewsBox.js
@@ -4,12 +4,12 @@ import "./NewsBox.css";
 const NewsBox = (props) => {
   return (
     <div className="newsBox">
-      <a className="imgContainer" href={props.url} target="_blank">
+      <a className="imgContainer" href={props.url} target="_blank" rel="noreferrer">
         <img src={props.urlToImage} alt={props.author} align="middle" />
       </a>
 
       <div className="dataContainer">
-        <a className="data" href={props.url} target="_blank">
+        <a className="data" href={props.url} target="_blank" rel="noreferrer">
           {props.title}
         </a>
       </div>
